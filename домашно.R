6879a45320a11df62dbeb92e2308064a616bc3b5
MelBusheva <101732899+MelBusheva@users.noreply.github.com>
2022-06-07 03:42:05 -0700

    Delete домашно.R

--- домашно.R
+++
@@ -1,6 +0,0 @@
-# Mariela Busheva
-# 200024
-# My three goals I want to achieve in the course "Quantitative methods in Finance"
-# 1. Pass the course with a good grade
-# 2. Learn how to work with R
-# 3. Analyse data more precisely
